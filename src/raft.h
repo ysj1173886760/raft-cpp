830c7fe0b1a7bf88dac7a3e4ad8897e0f30ccd96
ysj1173886760 <1173886760@qq.com>
2022-03-01 09:21:40 +0800

    complete raft structure

--- src/raft.h
+++ src/raft.h
@@ -2,36 +2,68 @@
 #include <atomic>
 #include <vector>
 #include <chrono>
+#include <queue>
+#include <string>
+
 #include "raft.pb.h"
 
-struct Log {
-    std::vector<Entry> Entries;
-    int Index0;
+struct CommitMessage {
+    std::string data;
+    int commitIndex;
+};
+
+class Log {
+public:
+    Log(): 
+        _entries(std::vector<Entry>{}),
+        _index0(0) {}
+
+private:
+    std::vector<Entry> _entries;
+    int _index0;
 };
 
 typedef std::chrono::time_point<std::chrono::steady_clock> MyTime;
 
-struct Raft {
+class Raft {
+public:
+    Raft() {
+
+    }
+
+private:
     // Lock to protect shared access to this peer's state
-    std::mutex Mu;
+    std::mutex _mu;
     // TODO: record RPC end points of all raft peers
 
     // TODO: Persister
 
     // this peer's index in RPC end points
-    int Me;
+    int _me;
 
     // set by Kill()
-    std::atomic<bool> Dead;
+    std::atomic<bool> _dead;
 
     // persist state
-    int VotedFor;
-    int CurrentTerm;
-    Log log;
+    int _votedFor;
+    int _currentTerm;
+    Log _log;
 
     // for election
-    int LeaderID;
-    int CurrentState;
-    MyTime ElectionTimer;
-    MyTime HeartbeatTimer;
+    int _leaderID;
+    int _currentState;
+    MyTime _electionTimer;
+    MyTime _heartbeatTimer;
+    
+    // for log
+    int _commitIndex;
+    int _lastApplied;
+    std::vector<int> _nextIndex;
+    std::vector<int> _matchIndex;
+
+    // for commit
+    // TODO: use a thread safe queue to mimic the channel
+    std::queue<CommitMessage> channel;
+
+    // TODO: snapshot
 };
